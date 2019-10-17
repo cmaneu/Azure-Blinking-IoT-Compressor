516668056724ceac698b45921d1f33e710ad47f7
Christopher Maneu <chmaneu@microsoft.com>
2019-10-17 12:02:30 +0200

    Refactor device twin parsing

---
+++ src/device-code/src/deviceTwin.h
@@ -0,0 +1,3 @@
+#include "DevKitMQTTClient.h"
+
+void parseTwinMessage(DEVICE_TWIN_UPDATE_STATE updateState, const char *message);
--- src/device-code/src/projectconfig.h
+++ src/device-code/src/projectconfig.h
@@ -2,3 +2,5 @@
 #define LOOP_INTERVAL 5000
 
 #define DEBUG_SERIAL
+
+#define FIRMWARE_VERSION "0.1"
